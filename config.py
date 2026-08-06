bc17a7da73dc2a1aafb72957270f34884ff21f32
Ganavi V C <ganavivc2@gmail.com>
2026-08-06 21:48:35 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
 MONGO_URI = os.getenv("MONGO_URI")
+
+# If not found in .env, use Streamlit secrets
+if not GROQ_API_KEY:
+    GROQ_API_KEY = st.secrets.get("GROQ_API_KEY")
+
+if not MONGO_URI:
+    MONGO_URI = st.secrets.get("MONGO_URI")
